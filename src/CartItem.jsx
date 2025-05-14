5f08c5f38ab67ea9496c9a0e07249d2ce72b778d
astaylor11413 <astaylor140@gmail.com>
2025-05-14 11:51:51 -0400

    added functionality supported by redux store

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,42 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach((item)=>{
+        total+=calculateTotalCost(item);
+    })
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity:item.quantity+1}));
   };
 
   const handleDecrement = (item) => {
+    if(item.quantity==1){
+        handleRemove(item);
+        dispatch(removeItem(item));
+    }else{
+        dispatch(updateQuantity({name: item.name, quantity:item.quantity-1}));
+    }
    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
+
+  const handleCheckoutShopping = (e) =>{
+    alert('Functionality to be added for future reference');
+  }
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+        return parseFloat(item.cost)*item.quantity;
   };
 
   return (
